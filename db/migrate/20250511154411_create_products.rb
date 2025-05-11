e65eeec1c60ab1d88a6a969588241c741258379d
rayane <rayquezia03@gmail.com>
2025-05-11 13:35:34 -0300

    initialized stock product control

---
+++ db/migrate/20250511154411_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[8.0]
+    def change
+      create_table :products do |t|
+        t.string :name
+        t.text :description
+        t.string :category
+        t.decimal :price, precision: 10, scale: 2
+        t.date :deleted_at
+        
+        t.timestamps
+      end
+    end
+  end
+  
