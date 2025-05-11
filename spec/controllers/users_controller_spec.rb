8c4c0628893269c4521a24617021851af75c29b7
Thallys <thallysribeiro37@gmail.com>
2025-05-11 15:38:26 -0300

    feat: add new users_controller_spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
-
+require 'api_helper'
 RSpec.describe UsersController, type: :controller do
-  FactoryBot.create(:user, cpf: "12345678901", name: "User 1", password: "password")
-  FactoryBot.create(:user, cpf: "12345678902", name: "User 2", password: "password")
+  include ApiHelper
+
+  let!(:user1) { FactoryBot.create(:user, name: "User 1", password: "123456") }
+  let!(:user2) { FactoryBot.create(:user, name: "User 2", password: "123456") }
+
+  let(:token) { generate_tokens(user1) }
+
   describe "GET /users" do
     it "returns a list of users" do
-      get :index
+      get :index, headers: { "Authorization" => "Bearer #{token}" }
       expect(response).to have_http_status(:ok)
       expect(response.body).to include("User 1")
       expect(response.body).to include("User 2")
