8a0c20d9f53e00aa9ac3846fad4ce6f353ca1a6a
Francisco <franciscocolatinodelima@gmail.com>
2025-05-11 17:53:07 -0300

    feat(login): config controllers to session and create env secret key base file and adjust jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   self.primary_key = 'cpf'
   has_secure_password
+
+  validates :cpf, presence: true
+  validates :cpf, uniqueness: { message: "já cadastrado" }
 end
