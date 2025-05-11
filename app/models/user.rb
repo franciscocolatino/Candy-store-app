f0cb13e4246366c8359fda746f4786cd8864554c
Thallys <thallysribeiro37@gmail.com>
2025-05-11 18:05:31 -0300

    feat: add validates in user model and add tests rspec user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   self.primary_key = 'cpf'
   has_secure_password
+
+  
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :cpf, presence: true, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be valid with valid attributes" do
+    user = FactoryBot.build(:user)
+    expect(user).to be_valid
+  end
+
+  it "should not be valid without a name" do
+    user = FactoryBot.build(:user, name: nil)
+    expect(user).not_to be_valid
+  end
+
+  it "should not be valid without a password" do
+    user = FactoryBot.build(:user, password: nil)
+    expect(user).not_to be_valid
+  end
 end
