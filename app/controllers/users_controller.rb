5b54688a99e79a611cc7715e435e7dc7c6bf4ec4
Thallys <thallysribeiro37@gmail.com>
2025-05-12 01:11:42 -0300

    feat: add rspec controller session and return values in json for list users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,14 @@ class UsersController < ApplicationController
   # GET /users or /users.json
   def index
     @users = User.all
+  
+    respond_to do |format|
+      format.html # renderiza views/users/index.html.erb
+      format.json { render json: @users }
+    end
   end
+  
+  
 
   # GET /users/1 or /users/1.json
   def show
