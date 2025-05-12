1a4dca7b64fc9269e30456f1af2f60da12670007
Thallys <thallysribeiro37@gmail.com>
2025-05-12 19:47:15 -0300

    feat: add models lots and products spec

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :product do
-    
+    name { Faker::Food.dish }
+    description { Faker::Food.description }
+    category { Faker::Food.spice }
+    price { Faker::Number.decimal(l_digits: 2, r_digits: 2) }
+
+    deleted_at { nil }
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be valid with valid attributes" do
+    product = FactoryBot.build(:product)
+    expect(product).to be_valid
+  end
+
+  it "should not be valid without a name" do
+    product = FactoryBot.build(:product, name: nil)
+    expect(product).not_to be_valid
+  end
+
+  it "should not be valid without a description" do
+    product = FactoryBot.build(:product, description: nil)
+    expect(product).not_to be_valid
+  end 
+
+  it "should not be valid without a category" do
+    product = FactoryBot.build(:product, category: nil)
+    expect(product).not_to be_valid
+  end
+
+  it "should not be valid without a price" do
+    product = FactoryBot.build(:product, price: nil)
+    expect(product).not_to be_valid
+  end
 end
