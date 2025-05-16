663839cae7df4e5857826e1da58be78758869c13
rayane <rayquezia03@gmail.com>
2025-05-16 13:54:06 -0300

    added warning expired produtcts

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
     has_many :lots, dependent: :destroy
 
-    validates :name, presence: true
+    validates :name, uniqueness: { case_sensitive: false, message: "do produto ja esta em uso" }
     validates :description, presence: true
     validates :category, presence: true
     validates :price, presence: true
