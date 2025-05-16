5fdcd290ebeb03f35ab0d4da6dab139cd155a30b
Thallys <thallysribeiro37@gmail.com>
2025-05-16 14:11:12 -0300

    fix: users return for default in is_admin is false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_11_161737) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_16_170844) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -38,7 +38,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_11_161737) do
   create_table "users", primary_key: "cpf", id: :string, force: :cascade do |t|
     t.string "name"
     t.string "password_digest"
-    t.boolean "is_admin"
+    t.boolean "is_admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
